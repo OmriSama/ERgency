3905fc04b0be3ca84921bde7bba0dca9762cf84a
grupozarateusa <grupozarateusa@gmail.com>
2016-11-15 22:11:13 -0800

    I created the EmergencyContact screen, make sure to redirect the continue button to the next screen. The continue button is currently looping back to the PersonalInformation screen.

---
+++ app/src/main/java/com/team3/ergency/Allergies.java
@@ -0,0 +1,22 @@
+package com.team3.ergency;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Allergies extends AppCompatActivity {
+
+    Button continueBt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setTitle("Allergies");
+        setContentView(R.layout.activity_allergies);
+
+        //Changes CONTINUE button color
+        continueBt = (Button) findViewById(R.id.allergies_continue);
+        continueBt.setBackgroundColor(Color.RED);
+        continueBt.setTextColor(Color.WHITE);
+    }
+}
