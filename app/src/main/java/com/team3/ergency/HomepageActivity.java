641e955a81f09a16ce728db549d1f761f57979ad
Howard Yu <kuanhy@uci.edu>
2016-11-16 03:16:31 -0800

    Merge remote-tracking branch 'remotes/ergency/master' into hy-maps
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/team3/ergency/HomepageActivity.java
+++ app/src/main/java/com/team3/ergency/HomepageActivity.java
@@ -1,7 +1,9 @@
 package com.team3.ergency;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomepageActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class HomepageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+    }
+
+    public void findHospitals(View view) {
+        Intent locationIntent = new Intent(this, LocationActivity.class);
+        startActivity(locationIntent);
+        finish();
     }
 }
