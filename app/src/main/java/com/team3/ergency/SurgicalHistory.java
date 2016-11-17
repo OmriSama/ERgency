cde56d3d17ec87e500ea379e581ec41de5a797c0
kimnguy <kimnguy@ymail.com>
2016-11-17 14:33:00 -0800

    Added extra button function

--- app/src/main/java/com/team3/ergency/SurgicalHistory.java
+++ app/src/main/java/com/team3/ergency/SurgicalHistory.java
@@ -1,22 +1,125 @@
 package com.team3.ergency;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.team3.ergency.fragment.DatePickerFragment;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static android.R.attr.id;
 
 public class SurgicalHistory extends AppCompatActivity {
 
+    ArrayList<EditText> surgeries;
+    ArrayList<EditText> surgeryDates;
+    FileOutputStream fileOut;
+    String fileName = "PatientInformation.txt";
     Button shNextBt;
+    private EditText surgery;
+    private EditText surgeryDate;
+    private LinearLayout linearLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setTitle("Surgical History");
         setContentView(R.layout.activity_surgical_history);
 
-        //Changes CONTINUE button color
-        shNextBt = (Button) findViewById(R.id.sh_next_bt);
-        shNextBt.setBackgroundColor(Color.RED);
-        shNextBt.setTextColor(Color.WHITE);
+        surgery = (EditText) findViewById(R.id.sh_surgery);
+        surgeryDate = (EditText) findViewById(R.id.sh_surgery);
+
+        surgeries = new ArrayList<EditText>();
+        surgeryDates = new ArrayList<EditText>();
+
+        surgeries.add(surgery);
+        surgeryDates.add(surgeryDate);
     }
+
+    public void launch_date_picker(View view) {
+        DatePickerFragment date_picker = new DatePickerFragment();
+        date_picker.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void addMore() {
+        // Create the layout for the surgery EditText
+        linearLayout = (LinearLayout) findViewById(R.id.sh_surgery_info);
+        LinearLayout.LayoutParams surgeryLayout = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        surgeryLayout.setMargins(5, 5, 5, 5);
+
+        // Create a new EditText for the surgery name
+        EditText newSurgery = new EditText(this);
+        newSurgery.setHint("Surgery");
+        newSurgery.setInputType(InputType.TYPE_CLASS_TEXT);
+        newSurgery.setLayoutParams(surgeryLayout);
+
+        surgeries.add(newSurgery);
+
+        // Create the layout for the surgery date
+        linearLayout = (LinearLayout) findViewById(R.id.sh_surgery_info);
+        LinearLayout.LayoutParams dateLayout = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        dateLayout.setMargins(5, 5, 5, 5);
+
+
+        // Create a new EditText for the surgery date
+        EditText newSurgeryDate = new EditText(this);
+        newSurgery.setHint("Date of Surgey");
+        newSurgery.setInputType(InputType.TYPE_CLASS_TEXT);
+        newSurgery.setLayoutParams(dateLayout);
+
+        surgeryDates.add(newSurgeryDate);
+    }
+
+    public void saveSurgeryInfo() {
+
+        // Open PatientInformation.txt in internal storage to store the patient information
+        try {
+            fileOut = openFileOutput(fileName, MODE_APPEND);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < surgeries.size(); i++) {
+            String surgeryInput = surgeries.get(i).getText().toString();
+            if (surgeryInput.length() < 0) {
+                break;
+            } else {
+                writeToFile("Surgery: " + surgeryInput + "\n");
+            }
+
+            surgeryInput = surgeryDates.get(i).getText().toString();
+            if (surgeryInput.length() < 0) {
+                Toast.makeText(getApplicationContext(), "Please fill in the surgery date", Toast.LENGTH_LONG).show();
+            } else {
+                writeToFile("Surgery Date: " + surgeryInput + "\n");
+            }
+        }
+
+        Intent intent = new Intent(this, Immunizations.class);
+        startActivity(intent);
+        finish();
+    }
+
+    // Write information to the file
+    public void writeToFile(String string) {
+        try {
+            fileOut.write(string.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
