14be9077b42508c33ef8d2c45bc3c7f8c8dc9e3f
Howard Yu <kuanhy@uci.edu>
2016-11-17 02:31:10 -0800

    Added functional confirmation page

--- app/src/main/java/com/team3/ergency/Confirmation.java
+++ app/src/main/java/com/team3/ergency/Confirmation.java
@@ -1,13 +1,42 @@
 package com.team3.ergency;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.team3.ergency.helper.FileHelper;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import static com.team3.ergency.helper.FileHelper.readFromFile;
 
 public class Confirmation extends AppCompatActivity {
+    private TextView hospitalInformationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
+
+        hospitalInformationTextView = (TextView) findViewById(R.id.conf_hospital_information);
+        displayHospital(hospitalInformationTextView);
+    }
+
+    private void displayHospital(TextView view) {
+        String hospitalInfo = FileHelper.readFromFile(this, getResources().getString(
+                R.string.output_file_hospital_information));
+
+        view.setText(hospitalInfo);
+    }
+
+    public void goBack(View view) {
+        Intent i = new Intent(this, HomepageActivity.class);
+        startActivity(i);
+        finish();
     }
 }
