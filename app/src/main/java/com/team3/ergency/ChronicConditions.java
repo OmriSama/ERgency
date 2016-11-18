7e0c6a1d75cedae5e280be92fc1d98c3873f7168
kimnguy <kimnguy@ymail.com>
2016-11-17 16:34:53 -0800

    Made some changes to chronic conditions

--- app/src/main/java/com/team3/ergency/ChronicConditions.java
+++ app/src/main/java/com/team3/ergency/ChronicConditions.java
@@ -96,25 +96,35 @@ public class ChronicConditions extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        // If the condition field is filled out, then a date must be required. Initially set to false
+        boolean requireDate = false;
+
         for (int i = 0; i < conditions_list.size(); i++) {
             String surgeryInput = conditions_list.get(i).getText().toString();
             if (surgeryInput.length() < 0) {
                 break;
             } else {
                 writeToFile("Chronic Condition: " + surgeryInput + "\n");
+                // Condition name was entered. Date of diagnosis is required
+                requireDate = true;
             }
 
             surgeryInput = dateofDiagnosis_list.get(i).getText().toString();
             if (surgeryInput.length() < 0) {
-                Toast.makeText(getApplicationContext(), "Please fill in the surgery date", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Please fill in the date of diagnosis", Toast.LENGTH_LONG).show();
             } else {
                 writeToFile("Date of Diagnosis: " + surgeryInput + "\n");
+                // Reset the variable
+                requireDate = false;
             }
         }
 
-        Intent intent = new Intent(this, SurgicalHistory.class);
-        startActivity(intent);
-        finish();
+        if (requireDate == false) {
+            Intent intent = new Intent(this, SurgicalHistory.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 
     // Write information to the file
